b7ca0314559374c4efdffd4a47c9bfd95c4b4929
IInspectable <github@smjh.de>
2021-07-21 20:56:02 +0200

    Tests assembly added

---
+++ Math-lib.Tests/VectorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Math_lib.Tests {
+
+    [TestFixture]
+    public class VectorTests {
+
+        [Test]
+        public void TestEmptyVector() {
+
+            var v = new Vector();
+            Assert.That(v.X, Is.EqualTo(0));
+            Assert.That(v.Y, Is.EqualTo(0));
+            Assert.That(v.Z, Is.EqualTo(0));
+        }
+    }
+}
