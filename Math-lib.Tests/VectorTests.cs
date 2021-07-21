3ff22c4242e3d98e820e8b092b74a3e20186bbfa
IInspectable <github@smjh.de>
2021-07-21 21:08:45 +0200

    tests

--- Math-lib.Tests/VectorTests.cs
+++ Math-lib.Tests/VectorTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+
+using NUnit.Framework;
 
 namespace Math_lib.Tests {
 
@@ -13,5 +15,35 @@ namespace Math_lib.Tests {
             Assert.That(v.Y, Is.EqualTo(0));
             Assert.That(v.Z, Is.EqualTo(0));
         }
+
+        [Test]
+        public void TestCtor1() {
+            var v = new Vector(3);
+            Assert.That(v.X, Is.EqualTo(3));
+            Assert.That(v.Y, Is.EqualTo(3));
+            Assert.That(v.Z, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void TestCtor2() {
+            var v = new Vector(1, 2, 3);
+            Assert.That(v.X, Is.EqualTo(1));
+            Assert.That(v.Y, Is.EqualTo(2));
+            Assert.That(v.Z, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void TestGetLength() {
+            var v = new Vector(3, 4, 5);
+            Assert.That(v.GetLength(), Is.EqualTo(Math.Sqrt(50)));
+        }
+
+        [Test]
+        public void TestGetLengthSqrt() {
+            var v = new Vector(3, 4, 5);
+            Assert.That(v.GetLengthSqrt(), Is.EqualTo(50));
+        }
+
     }
+
 }
