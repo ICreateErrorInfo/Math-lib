45862d28b51e706f4ed3414bdf0f3ed2b81062fb
IInspectable <github@smjh.de>
2021-11-02 16:12:15 +0100

    argument names

--- RasterizerTest/MainWindow.xaml.cs
+++ RasterizerTest/MainWindow.xaml.cs
@@ -14,14 +14,14 @@ namespace RasterizerTest
             
             Rasterizer r = new Rasterizer(1920, 1080);
 
-            //r.DrawLine(new(r.width - 1,0), new(0, r.height - 1), System.Drawing.Color.Red, 10);
+            r.DrawLine(p1: new(r.width - 1,0), p2: new(0, r.height - 1), c: System.Drawing.Color.Red, thickness: 10);
             //r.DrawLine(new(0,0), new(r.width - 1, r.height - 1), System.Drawing.Color.FromArgb(226, 7, 255), 10);
             //r.DrawLine(new(0,r.height/2), new(r.width - 1, r.height/2), System.Drawing.Color.Cyan, 10);
             //r.DrawLine(new(r.width/2,0), new(r.width/2, r.height - 1), System.Drawing.Color.Green, 10);
 
-            r.DrawCircle(new(r.width/2, r.height/2), 30, System.Drawing.Color.White, true);
+            r.DrawCircle(p1: new(r.width/2.0, r.height/2.0), radius: 30, c: System.Drawing.Color.White, fill: true);
 
-            r.DrawCircle(new(0,0), 30, System.Drawing.Color.White, true); // Bug
+            r.DrawCircle(p1: new(0,0), radius: 30, c: System.Drawing.Color.White, fill: true); // Bug
 
             Display.Source = r.GetSource();
         }
