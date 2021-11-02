0df0e3ce1988ebbd4bbcc9854182e2d60e67ddba
IInspectable <github@smjh.de>
2021-11-02 16:14:35 +0100

    cleanup

--- RasterizerTest/MainWindow.xaml.cs
+++ RasterizerTest/MainWindow.xaml.cs
@@ -15,9 +15,9 @@ namespace RasterizerTest
             Rasterizer r = new Rasterizer(1920, 1080);
 
             r.DrawLine(p1: new(r.width - 1,0), p2: new(0, r.height - 1), c: System.Drawing.Color.Red, thickness: 10);
-            //r.DrawLine(new(0,0), new(r.width - 1, r.height - 1), System.Drawing.Color.FromArgb(226, 7, 255), 10);
-            //r.DrawLine(new(0,r.height/2), new(r.width - 1, r.height/2), System.Drawing.Color.Cyan, 10);
-            //r.DrawLine(new(r.width/2,0), new(r.width/2, r.height - 1), System.Drawing.Color.Green, 10);
+            r.DrawLine(p1: new(0,0), p2: new(r.width - 1, r.height - 1), c: System.Drawing.Color.FromArgb(226, 7, 255), thickness: 10);
+            r.DrawLine(p1: new(0,r.height/2.0), p2: new(r.width - 1, r.height/2.0), c: System.Drawing.Color.Cyan, thickness: 10);
+            r.DrawLine(p1: new(r.width/2.0,0.0), p2: new(r.width/2.0, r.height - 1), c: System.Drawing.Color.Green, thickness: 10);
 
             r.DrawCircle(p1: new(r.width/2.0, r.height/2.0), radius: 30, c: System.Drawing.Color.White, fill: true);
 
