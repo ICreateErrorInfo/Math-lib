f2cc41eeae90ead975dfff3972c727656b93e0ef
IInspectable <github@smjh.de>
2021-11-06 10:37:31 +0100

    Dynamic Rasterizer size

--- RasterizerTest/MainWindow.xaml.cs
+++ RasterizerTest/MainWindow.xaml.cs
@@ -6,20 +6,27 @@ namespace RasterizerTest
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();  
-            
-            Rasterizer r = new Rasterizer(1920, 1080, 18, true);
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo) 
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+             
+            var r = new Rasterizer(width: (int)ActualWidth, height: (int)ActualHeight, scale: 18, CooMi: true);
 
             //r.DrawLine(p1: new(0,0), p2: new(2, 0), c: System.Drawing.Color.Red, thickness: 1);
             //r.DrawLine(p1: new(0,0), p2: new(r.width - 1, r.height - 1), c: System.Drawing.Color.FromArgb(226, 7, 255), thickness: 10);
             //r.DrawLine(p1: new(0,r.height/2.0), p2: new(r.width - 1, r.height/2.0), c: System.Drawing.Color.Cyan, thickness: 10);
             //r.DrawLine(p1: new(r.width/2.0,0.0), p2: new(r.width/2.0, r.height - 1), c: System.Drawing.Color.Green, thickness: 10);
 
-            //r.DrawCircle(p1: new(r.width/2.0, r.height/2.0), radius: 30, c: System.Drawing.Color.White, fill: true);
+            // r.DrawCircle(p1: new(r.width/2.0, r.height/2.0), radius: 30, c: System.Drawing.Color.White, fill: true);
             //
             //r.DrawCircle(p1: new(0,0), radius: 30, c: System.Drawing.Color.White, fill: true);
 
@@ -33,5 +40,7 @@ namespace RasterizerTest
 
             Display.Source = r.GetSource();
         }
+
+      
     }
 }
