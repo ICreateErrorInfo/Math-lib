9780864d3e6342548d6437466a07901aeb38fd87
ICreateErrorInfo <hm.Github@smjh.de>
2022-09-03 16:19:45 +0200

    Vector3D

--- MoarxMath/Point3D.cs
+++ MoarxMath/Point3D.cs
@@ -2,8 +2,9 @@
 
 namespace MoarxMath
 {
-    public record Point3D<T>(T X, T Y, T Z) where T : IAdditionOperators<T, T, T>
+    public record Point3D<T>(T X, T Y, T Z) where T : IAdditionOperators<T, T, T>, ISubtractionOperators<T, T, T>
     {
         public static Point3D<T> operator +(Point3D<T> left, Point3D<T> right) => left with { X = left.X + right.X, Y = left.Y + right.Y, Z = left.Z + right.Z };
+        public static Vector3D<T> operator -(Point3D<T> left, Point3D<T> right) => new Vector3D<T>(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
     }
 }
---
+++ MoarxMath/Vector3D.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Numerics;
+
+namespace MoarxMath
+{
+    public record Vector3D<T>(T X, T Y, T Z) where T : IAdditionOperators<T, T, T>, ISubtractionOperators<T, T, T>
+    { 
+        public static Vector3D<T> operator +(Vector3D<T> left, Vector3D<T> right) => left with { X = left.X + right.X, Y = left.Y + right.Y, Z = left.Z + right.Z };
+        public static Vector3D<T> operator -(Vector3D<T> left, Vector3D<T> right) => left with { X = left.X + right.X, Y = left.Y + right.Y, Z = left.Z + right.Z };
+    }
+}
