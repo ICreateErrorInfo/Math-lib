3af1e04ae47fc381180190695061a59df68390c6
ICreateErrorInfo <hm.Github@smjh.de>
2022-09-03 16:42:02 +0200

    Vector and Point operator overload

--- MoarxMath/Point3D.cs
+++ MoarxMath/Point3D.cs
@@ -2,9 +2,38 @@
 
 namespace MoarxMath
 {
-    public record Point3D<T>(T X, T Y, T Z) where T : IAdditionOperators<T, T, T>, ISubtractionOperators<T, T, T>
+    public record Point3D<T>(T X, T Y, T Z) where T : IAdditionOperators<T, T, T>, ISubtractionOperators<T, T, T>, INumber<T>
     {
-        public static Point3D<T> operator +(Point3D<T> left, Point3D<T> right) => left with { X = left.X + right.X, Y = left.Y + right.Y, Z = left.Z + right.Z };
-        public static Vector3D<T> operator -(Point3D<T> left, Point3D<T> right) => new Vector3D<T>(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
+        public static Point3D<T> operator +(Point3D<T> left, Point3D<T> right) => left with 
+        { 
+            X = left.X + right.X,
+            Y = left.Y + right.Y,
+            Z = left.Z + right.Z 
+        };
+        public static Point3D<T> operator +(Vector3D<T> left, Point3D<T> right) => right with
+        {
+            X = left.X + right.X,
+            Y = left.Y + right.Y,
+            Z = left.Z + right.Z
+        };
+        public static Point3D<T> operator +(Point3D<T> left, Vector3D<T> right) => left with
+        {
+            X = left.X + right.X,
+            Y = left.Y + right.Y,
+            Z = left.Z + right.Z
+        };
+
+        public static Vector3D<T> operator -(Point3D<T> left, Point3D<T> right) => new Vector3D<T>
+        (
+            left.X - right.X,
+            left.Y - right.Y,
+            left.Z - right.Z
+        );
+        public static Point3D<T> operator -(Point3D<T> left, Vector3D<T> right) => left with
+        {              
+            X = left.X - right.X,
+            Y = left.Y - right.Y,
+            Z = left.Z - right.Z
+        };
     }
 }
--- MoarxMath/Vector3D.cs
+++ MoarxMath/Vector3D.cs
@@ -3,9 +3,29 @@ using System.Numerics;
 
 namespace MoarxMath
 {
-    public record Vector3D<T>(T X, T Y, T Z) where T : IAdditionOperators<T, T, T>, ISubtractionOperators<T, T, T>
+    public record Vector3D<T>(T X, T Y, T Z) where T : IAdditionOperators<T, T, T>, ISubtractionOperators<T, T, T>, IMultiplyOperators<T, T, T>, INumber<T>
     { 
-        public static Vector3D<T> operator +(Vector3D<T> left, Vector3D<T> right) => left with { X = left.X + right.X, Y = left.Y + right.Y, Z = left.Z + right.Z };
-        public static Vector3D<T> operator -(Vector3D<T> left, Vector3D<T> right) => left with { X = left.X + right.X, Y = left.Y + right.Y, Z = left.Z + right.Z };
+        public static Vector3D<T> operator +(Vector3D<T> left, Vector3D<T> right) => left with 
+        {
+            X = left.X + right.X,
+            Y = left.Y + right.Y,
+            Z = left.Z + right.Z 
+        };
+
+        public static Vector3D<T> operator -(Vector3D<T> left, Vector3D<T> right) => left with 
+        { 
+            X = left.X - right.X,
+            Y = left.Y - right.Y,
+            Z = left.Z - right.Z 
+        };
+
+        public static T operator *(Vector3D<T> left, Vector3D<T> right) => (left.X * right.X) + (left.Y * right.Y);
+
+        public static Vector3D<T> operator /(Vector3D<T> left, T scalar) => left with
+        {
+            X = left.X / scalar,
+            Y = left.Y / scalar,
+            Z = left.Z / scalar
+        };
     }
 }
