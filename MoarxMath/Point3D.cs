1e3f13e05596fccf2ae7db85e3fb6870913a4d30
ICreateErrorInfo <hm.Github@smjh.de>
2022-09-03 16:03:35 +0200

    Point3D

--- MoarxMath/Point3D.cs
+++ MoarxMath/Point3D.cs
@@ -1,7 +1,9 @@
-﻿namespace MoarxMath
-{
-    public record Point3D
-    {
+﻿using System.Numerics;
 
+namespace MoarxMath
+{
+    public record Point3D<T>(T X, T Y, T Z) where T : IAdditionOperators<T, T, T>
+    {
+        public static Point3D<T> operator +(Point3D<T> left, Point3D<T> right) => left with { X = left.X + right.X, Y = left.Y + right.Y, Z = left.Z + right.Z };
     }
 }
