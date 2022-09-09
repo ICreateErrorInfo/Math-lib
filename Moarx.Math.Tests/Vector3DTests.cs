86f3ac8b9415fa71b4dba9c533e9b719786723c9
IInspectable <github@smjh.de>
2022-09-09 13:25:06 +0200

    Vector3DTests

---
+++ Moarx.Math.Tests/Vector3DTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Moarx.Math.Tests;
+
+[TestFixture]
+public class Vector3DTests {
+
+    [Test]
+    public void TestLength() {
+
+        var v = new Vector3D<double>(3, 4, 5);
+
+        Assert.That(v.GetLengthSquared(), Is.EqualTo(3 * 3 + 4 * 4 + 5 * 5));
+        Assert.That(v.GetLength(),        Is.EqualTo(System.Math.Sqrt(50)));
+    }
+
+}
