55d74a98e4a0b0130fc6cb3e9d920cd6036dd1a9
ICreateErrorInfo <hm.Github@smjh.de>
2022-12-01 21:16:42 +0100

    Bounds3D

---
+++ Moarx.Math.Tests/Bounds3DTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace Moarx.Math.Tests; 
+[TestFixture]
+public class Bounds3DTests {
+    [Test]
+    public void TestSyntax() {
+        var b = new Bounds3D<double> { PMin = new Point3D<double>(1, 1, 1), PMax = new Point3D<double>(2, 2, 2) };
+
+        Assert.That(b.PMin, Is.EqualTo(new Point3D<double>(1, 1, 1)));
+        Assert.That(b.PMax, Is.EqualTo(new Point3D<double>(2, 2, 2)));
+    }
+    [Test]
+    public void TestEmptyBound3D() {
+        var b = new Bounds3D<double>();
+        Assert.That(b.PMin, Is.EqualTo(new Point3D<double>(double.MaxValue)));
+        Assert.That(b.PMax, Is.EqualTo(new Point3D<double>(double.MinValue)));
+    }
+    [Test]
+    public void TestCtor1() {
+        Point3D<double> p = new Point3D<double>(1);
+        var b = new Bounds3D<double>(p);
+
+        Assert.That(b.PMin, Is.EqualTo(p));
+        Assert.That(b.PMax, Is.EqualTo(p));
+    }
+    [Test]
+    public void TestCtor2() {
+        Point3D<double> p1 = new Point3D<double>(1);
+        Point3D<double> p2 = new Point3D<double>(7);
+
+        var b = new Bounds3D<double>(p1, p2);
+
+        Assert.That(b.PMin, Is.EqualTo(p1));
+        Assert.That(b.PMax, Is.EqualTo(p2));
+    }
+}
---
+++ Moarx.Math/Bounds3D.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+namespace Moarx.Math;
+public readonly record struct Bounds3D<T>
+where T : struct, INumber<T> {
+
+    readonly Point3D<T> _pMin, _pMax;
+
+    public Bounds3D() {
+        T minNum = T.CreateChecked(double.MinValue);
+        T maxNum = T.CreateChecked(double.MaxValue);
+        _pMin = new Point3D<T>(maxNum);
+        _pMax = new Point3D<T>(minNum);
+    }
+    public Bounds3D(Point3D<T> p) {
+        PMin = p;
+        PMax = p;
+    }
+    public Bounds3D(Point3D<T> p1, Point3D<T> p2) {
+        //TODO
+        //PMin = Point3D<T>.Min(p1, p2);
+        //PMax = Point3D<T>.Max(p1, p2);
+    }
+
+    public Point3D<T> PMin {
+        get => _pMin;
+        init {
+            if (T.IsNaN(value.X) || T.IsNaN(value.Y) || T.IsNaN(value.Z)) {
+                throw new ArgumentOutOfRangeException(nameof(value), "Bounds data has NaN");
+            }
+            _pMin = value;
+        }
+    }
+    public Point3D<T> PMax {
+        get => _pMax;
+        init {
+            if (T.IsNaN(value.X) || T.IsNaN(value.Y) || T.IsNaN(value.Z)) {
+                throw new ArgumentOutOfRangeException(nameof(value), "Bounds data has NaN");
+            }
+            _pMax = value;
+        }
+    }
+
+    //TODO Method implementation
+
+    public Point3D<T> this[int i] {
+        get {
+            if (i == 0) {
+                return _pMin;
+            }
+            if (i == 1) {
+                return _pMax;
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+    }
+}
