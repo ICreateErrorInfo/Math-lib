2b5dacd6b5ab2bd7fa80c46062b465f834adcc4a
ICreateErrorInfo <hm.Github@smjh.de>
2023-01-25 18:57:18 +0100

    Render

--- RasterizerTest/MainWindow.xaml.cs
+++ RasterizerTest/MainWindow.xaml.cs
@@ -3,12 +3,19 @@ using System.Windows;
 using System.Windows.Media.Imaging;
 using System.Windows.Media;
 using Moarx.Math;
+using System.Windows.Threading;
+using System;
+using System.Drawing;
+using System.Diagnostics;
 
 namespace RasterizerTest {
 
     public partial class MainWindow: Window {
 
-        //TODO implement new Rasterizer
+        private readonly DispatcherTimer   _timer;
+        private DirectGraphics _graphics;
+        private DirectBitmap _bitmap;
+
         public MainWindow() {
             InitializeComponent();
 
@@ -16,12 +23,21 @@ namespace RasterizerTest {
             int height        = 513;
             int bytesPerPixel = 4;
 
-            byte[] data = new byte[width * height * bytesPerPixel];
+            _bitmap = DirectBitmap.Create(width: width, height: height, bytesPerPixel);
 
-            var bmp = DirectBitmap.Create(width: width, height: height, data, bytesPerPixel);
-            bmp.Clear(DirectColors.Gray);
+            _graphics = DirectGraphics.Create(_bitmap);
 
-            var graphics = DirectGraphics.Create(bmp);
+            _timer = new DispatcherTimer {
+                Interval = TimeSpan.FromMilliseconds(1),
+                IsEnabled = true
+            };
+
+            _timer.Tick += UpdateImage;
+        }
+
+        private void UpdateImage(object sender, EventArgs e) {
+            _bitmap.Clear(DirectColors.Gray);
+            Stopwatch stopwatch = Stopwatch.StartNew();
 
             //graphics.DrawAntiAliasedLine(new(new(1, 1), new(200, 198)), DirectColors.Black);
             //graphics.DrawAntiAliasedEllipse(new(new(250, 250), 100, 100), DirectColors.White);
@@ -39,42 +55,17 @@ namespace RasterizerTest {
             //graphics.DrawTriangleFilled(new(new(10, 10), new(100, 200), new(300, 300)), DirectColors.Black);
             //graphics.DrawAntiAliasedTriangleFilled(new(new(10, 10), new(100, 200), new(300, 300)), DirectColors.Black);
             //graphics.DrawTriangle(new(new(10, 10), new(100, 200), new(300, 300)), DirectColors.Black);
-            graphics.DrawSSAATriangleFilled2(new(new(10, 10), new(100, 200), new(300, 300)),4, DirectColors.White);
+            _graphics.DrawMSAATriangleFilled(new(new(10, 10), new(100, 200), new(300, 300)), 8, DirectColors.White);
             //graphics.DrawThickLine(new(new(20, 20), new(200, 400)), 2, DirectColors.Tan);
             //graphics.DrawSSAALine(new(new(20, 20), new(200, 400)), 2, 4, DirectColors.Tan);
             //graphics.DrawRectangle(new(new(20, 20), new(150, 180)), DirectColors.Aqua);
 
-            Display.Source = ToImageSource(bmp);
+            Display.Source = ToImageSource(_bitmap);
+            Time.Text = stopwatch.ElapsedMilliseconds.ToString() + "ms";
         }
 
         protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo) {
             base.OnRenderSizeChanged(sizeInfo);
-            //r.UpdateSize((int)sizeInfo.NewSize.Width, (int)sizeInfo.NewSize.Height);
-
-            //r.DrawLine(p1: new(0,0), p2: new(2, 0), c: System.Drawing.Color.Red, thickness: 1);
-
-            //r.DrawCircle(p1: new(0,0), radius: 1, c: System.Drawing.Color.White, fill: true);
-
-            //r.DrawCircle(new(0,0), 1, System.Drawing.Color.White, true);
-            //r.DrawLine(new(-1,-1), new(1,1), System.Drawing.Color.White);
-
-            //r.DrawRectangle(new(-1, 1), new(1, -1), System.Drawing.Color.White, true);
-            //r.DrawLine(new(-1, 1), new(1, -1), System.Drawing.Color.White, 1);
-
-            //r.DrawLine(new(0, 0), new(80, 8), System.Drawing.Color.White, 0.1);
-
-            //r.DrawPoint(new(0.5,0.5), System.Drawing.Color.White);
-
-            //r.DrawEllipse(new(0,0), 19, 2, System.Drawing.Color.White, true);
-
-            //r.DrawQuadBezier(new(0,0), new(0, 2), new(2,2), System.Drawing.Color.White);
-
-            //r.DrawPoint(new(0.5,0.5), System.Drawing.Color.White);
-            //r.DrawTriangle(new(0,0), new(1,0), new(2,-2), System.Drawing.Color.AliceBlue);
-
-            //var i = Vector3D.Normalize(new Vector3D(3, 4, 5)).GetLength();
-
-            //Display.Source = r.GetSource();
         }
 
         public static ImageSource ToImageSource(DirectBitmap bitmap) {
