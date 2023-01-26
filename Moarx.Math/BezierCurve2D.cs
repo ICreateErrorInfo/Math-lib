1de13f5367e9fc5b2d1569fbe7b181a0c5e8994b
ICreateErrorInfo <hm.Github@smjh.de>
2023-01-26 17:52:20 +0100

    Bezier curve

---
+++ Moarx.Math/BezierCurve2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace Moarx.Math;
+
+public static class BezierCurve2D {
+    public static BezierCurve2D<T> Create<T>(Point2D<T> startPoint, Point2D<T> point1, Point2D<T> point2, Point2D<T> endPoint) where T : struct, INumber<T> {
+        return new BezierCurve2D<T>(startPoint, point1, point2, endPoint);
+    }
+}
+public readonly record struct BezierCurve2D<T> 
+    where T : struct, INumber<T> {
+
+    public BezierCurve2D(Point2D<T> startPoint, Point2D<T> point1, Point2D<T> point2, Point2D<T> endPoint) {
+        StartPoint = startPoint;
+        Point1 = point1;
+        Point2 = point2;
+        EndPoint = endPoint;
+    }
+
+    public Point2D<T> StartPoint { get; }
+    public Point2D<T> Point1 { get; }
+    public Point2D<T> Point2 { get; }
+    public Point2D<T> EndPoint { get; }
+}
